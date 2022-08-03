ee2f52e803094052cc9bca56ce668549e2f3500e
Jorden-Brown <jordenbrown17@gmail.com>
2022-08-03 14:08:58 -0400

    8/3 commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.springframework.format.datetime.joda.DateTimeParser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class MainController {
+
+	@RequestMapping(method = RequestMethod.GET, value="/time")
+	public String getTime(){
+		Date date = new Date(System.currentTimeMillis());		
+		
+	    SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+	    formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+	    return formatter.format(date);
+	}
+	
+	
+	 // { "name":"Etc/GMT+9", "tz":"Etc/GMT+9" } 
+	
+	@RequestMapping(method = RequestMethod.POST, value="/Localtime")
+	// @ResponseBody
+	public String getLocalTime(@RequestBody timeZone tz){ 
+		ZonedDateTime time1 = ZonedDateTime.now(ZoneId.of(tz.getTz()));
+		String format = "yyyy-MM-dd 'at' HH:mm:ss z"; 
+	    //SimpleDateFormat formatter= new SimpleDateFormat(formatter);
+	    //formatter.setTimeZone(TimeZone.getTimeZone(tz.getTz()));
+	    return time1.format(DateTimeFormatter.ofPattern(format));//formatter.format(Date.from(time1.toInstant(ZoneOffset.)));
+	}
+	
+	
+	// taking in a body request "timezone" using json 
+	@PostMapping("/UTC")
+	public ZonedDateTime getUTCTime(@RequestBody String tz){
+		Date date = new Date(System.currentTimeMillis());	
+		
+		LocalDateTime time = LocalDateTime.now();
+		ZonedDateTime time2 = ZonedDateTime.now(ZoneId.of(tz));
+		
+		return time2;
+	    
+	}
+	
+	
+	
+	@GetMapping("/hello")
+	public String base() {
+		return"hello world";
+	}
+}
---
+++ src/main/java/com/example/demo/timeZone.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+public class timeZone {
+
+	private String tz;
+	private String name;
+	public String getTz() {
+		return tz;
+	}
+	public void setTz(String tz) {
+		this.tz = tz;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	} 
+	
+	
+	
+	
+}
